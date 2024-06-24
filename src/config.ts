4698eee23bf2b2e17d0e20863d6f99d630c6652f
Abdulmalek Alsalmi <148875378+AbdulmalekAI@users.noreply.github.com>
2024-06-24 11:01:58 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export default {
   secretKey: 'THISISMYSECURETOKEN',
   EMAIL: 'admin@admin.com',
-  PASSWORD: 'BotFin@2023',
+  PASSWORD: '',
   BOT_URL: 'https://bot.fintechsys.net',
   BOT_ID: 'jawallibot',
   host: 'http://localhost',
